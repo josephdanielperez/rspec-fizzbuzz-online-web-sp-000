a073407d68c43a0bdb1ed000bac892eb7ff61cd4
Joseph Perez <hello@josephdanielperez.com>
2020-06-12 21:23:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,8 +6,10 @@ def fizzbuzz(int)
     return "Fizz"
   elsif (int % 5 == 0)
     return "Buzz"
-  else (int % 3 == 0) && (int % 5 == 0)
+  elsif (int % 3 == 0) && (int % 5 == 0)
     return "FizzBuzz"
+  else (int !% 3 == 0) || (int !% 5 == 0)
+    return false
   end
 end
 
