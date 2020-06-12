2f03c2192ef42579284c099c1d025850999be23e
Joseph Perez <hello@josephdanielperez.com>
2020-06-12 21:19:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,10 +2,10 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0
+  if (int % 3 == 0)
     return "Fizz"
   end
-  elsif int % 5 == 0
+elsif (int % 5 == 0)
     return "Buzz"
   end
   else (int % 3 == 0) && (int % 5 == 0)
